0da2f2f4d833c2c034091fd16cf840dc0ef6d33a
Aditya Gupta <adityahupta1605@gmail.com>
2025-11-28 08:46:56 +0530

    commit

--- Library-Management-main/frontend/src/utils/config.js
+++ Library-Management-main/frontend/src/utils/config.js
@@ -1,3 +1,1 @@
-// export const Server_URL  = 'http://localhost:5000/'
-//export const Server_URL  = import.meta.env.VITE_BACKEND_URL;
-export const Server_URL  = 'http://localhost:5000/'
+export const Server_URL  = 'https://library-managment-system-0i38.onrender.com/'
